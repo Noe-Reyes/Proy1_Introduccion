5a0914ecb03e80880d9743e1ffd9c550ede9ace8
Noe-Reyes <reyes.noe007@hotmail.com>
2025-12-01 14:52:45 -0600

    Modificación de texto para enseñarle al HUGO

--- and_gate.v
+++ and_gate.v
@@ -8,4 +8,5 @@ module and_gate
     // Aun no le entiendo al pinche programa.
     //Creo que al menos lo mas facil ya lo se hacer.
     // Vamos a probar otra modificacion, para saber como funciona.
+    // Asi se hace un cambio.
 endmodule
